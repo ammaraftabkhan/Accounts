148dfc900e30a0ce03525f29b6f6f821077be43c
Ammar Aftab Khan <ammaraftabkhan@gmail.com>
2023-03-13 18:25:43 +0500

    Fiscal Year CRUD

---
+++ Accounts.Services/Services/IAccountFiscalYearServices.cs
@@ -0,0 +1,19 @@
+﻿using Accounts.Common.Virtual_Models;
+using Accounts.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Services.Services
+{
+    public interface IAccountFiscalYearServices
+    {
+        bool AddFiscalYear(VM_AccountFiscalYear _VM_AccountFiscalYear);
+        List<AccountFiscalYear> GetAllFiscalYear();
+        AccountFiscalYear FindFiscalYear(long id);
+        bool UpdateFiscalYear(VM_AccountFiscalYear _VM_AccountFiscalYear);
+        bool DeleteFiscalYear(int id);
+    }
+}
