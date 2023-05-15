be76210225d8e9e76384a13d64f2479245e6f243
Ammar Aftab Khan <ammaraftabkhan@gmail.com>
2023-05-15 17:03:24 +0500

    validator removed

--- Accounts.Common/Validator/Accounts_Val.cs
+++ Accounts.Common/Validator/Accounts_Val.cs
@@ -1,64 +1,64 @@
-﻿using Accounts.Common.Virtual_Models;
-using Accounts.Core.Models;
-using FluentValidation;
-using NPOI.SS.Formula.Functions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Accounts.Common.Virtual_Models;
+//using Accounts.Core.Models;
+//using FluentValidation;
+//using NPOI.SS.Formula.Functions;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Accounts.Common.Validator
-{
-    public class AccountHeadType_Val : AbstractValidator<VM_AccountHeadType>
-    {
-        public AccountHeadType_Val() 
-        {
+//namespace Accounts.Common.Validator
+//{
+//    public class AccountHeadType_Val : AbstractValidator<VM_AccountHeadType>
+//    {
+//        public AccountHeadType_Val() 
+//        {
 
-            RuleFor(x => x.AcHeadTypeName).NotEmpty();
-
-
-
-        }
-    }
-    public class AccountHead_Val : AbstractValidator<VM_AccountHeads>
-    {
-        public AccountHead_Val()
-        {
-            RuleFor(x => x.AcHeadName).NotEmpty();
-            RuleFor(x => x.AcHeadTypeId).NotEmpty();
-
-        }
-    }
-    public class AccountControl_Val : AbstractValidator<VM_AccountControl>
-    {
-        public AccountControl_Val()
-        {
-            RuleFor(x => x.AcControlName).NotEmpty();
-            RuleFor(x => x.AcHeadId).NotEmpty();
-
-        }
-    }
-    public class AccountLedger_Val : AbstractValidator<VM_AccountLedger>
-    {
-        public AccountLedger_Val()
-        {
-            RuleFor(x => x.AcLedgerName).NotEmpty();
-            RuleFor(x => x.AcControlId).NotEmpty();
+//            RuleFor(x => x.AcHeadTypeName).NotEmpty();
 
 
-        }
-    }
-    public class AccountSubLedger_Val : AbstractValidator<VM_AccountSubLedger>
-    {
-        public AccountSubLedger_Val()
-        {
-            RuleFor(x => x.AcSubLedgerName).NotEmpty();
-            RuleFor(x => x.AcLedgerId).NotEmpty();
+
+//        }
+//    }
+//    public class AccountHead_Val : AbstractValidator<VM_AccountHeads>
+//    {
+//        public AccountHead_Val()
+//        {
+//            RuleFor(x => x.AcHeadName).NotEmpty();
+//            RuleFor(x => x.AcHeadTypeId).NotEmpty();
+
+//        }
+//    }
+//    public class AccountControl_Val : AbstractValidator<VM_AccountControl>
+//    {
+//        public AccountControl_Val()
+//        {
+//            RuleFor(x => x.AcControlName).NotEmpty();
+//            RuleFor(x => x.AcHeadId).NotEmpty();
+
+//        }
+//    }
+//    public class AccountLedger_Val : AbstractValidator<VM_AccountLedger>
+//    {
+//        public AccountLedger_Val()
+//        {
+//            RuleFor(x => x.AcLedgerName).NotEmpty();
+//            RuleFor(x => x.AcControlId).NotEmpty();
 
 
-        }
-    }
+//        }
+//    }
+//    public class AccountSubLedger_Val : AbstractValidator<VM_AccountSubLedger>
+//    {
+//        public AccountSubLedger_Val()
+//        {
+//            RuleFor(x => x.AcSubLedgerName).NotEmpty();
+//            RuleFor(x => x.AcLedgerId).NotEmpty();
 
-}
+
+//        }
+//    }
+
+//}
